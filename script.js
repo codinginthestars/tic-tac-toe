139f7f4e021d954b133b4353defc43faf18e4db1
William <codinginthestars@gmail.com>
2024-08-31 03:25:03 -0500

    batman

---
+++ script.js
@@ -0,0 +1,9 @@
+function GameBoard() {
+    const board = ["","","","","","","","",""];
+
+    const getBoard = () => board;
+
+    const resetBoard = () => {
+        board = ["","","","","","","","",""]
+    }
+}
